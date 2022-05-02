09fb529438687c548b5d3b47ae52f993c2f413d8
scratch-cmd <99659958+scratch-cmd@users.noreply.github.com>
2022-05-02 20:01:59 +0800

    Create pwdtest.js

---
+++ pwdtest.js
@@ -0,0 +1,8 @@
+function yanz() {
+  if(document.getElementById("usm").value=="肚"){
+    if(document.getElementById("pwd").value=="hellofn!@#"){
+        alert('欢迎来到肚子世界！');
+        location.replace('https://scratch-cmd.github.io/clientdisplay/inbellycraft');
+  }}else{
+    alert('你的身份不对啊');
+}}
