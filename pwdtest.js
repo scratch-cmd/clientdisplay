76bf32de709e25161783310b789403280fafeafb
scratch-cmd <99659958+scratch-cmd@users.noreply.github.com>
2022-05-03 13:32:05 +0800

    Update pwdtest.js

--- pwdtest.js
+++ pwdtest.js
@@ -1,8 +1,9 @@
 function yanz() {
-  if(document.getElementById("usm").value=="肚"){
-    if(document.getElementById("pwd").value=="hellofn!@#"){
-        location.replace('https://scratch-cmd.github.io/clientdisplay/inbellycraft');
-        alert('欢迎来到肚子世界！');
+  wifi()
+    if(document.getElementById("usm").value=="肚"){
+      if(document.getElementById("pwd").value=="hellofn!@#"){
+          location.replace('https://scratch-cmd.github.io/clientdisplay/inbellycraft');
+          alert('欢迎来到肚子世界！');
   }}else{
     alert('你的身份不对啊');
 }}
