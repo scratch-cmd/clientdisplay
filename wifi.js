52d0e5141e47a8b79e1197c45488098daf9762fd
scratch-cmd <99659958+scratch-cmd@users.noreply.github.com>
2022-05-03 13:33:30 +0800

    Update wifi.js

--- wifi.js
+++ wifi.js
@@ -1,6 +1,4 @@
 function wifi(){
-if(window.navigator.onLine==true){
-location.replace('https://scratch-cmd.github.io/clientdisplay');
-}else{
-alert('请保持网络连接再登录！');
+if(window.navigator.onLine==false){
+  alert('请保持网络连接再登录！');
 }}
