7318a1880424b9c547f0738284e7bed061ecb607
scratch-cmd <99659958+scratch-cmd@users.noreply.github.com>
2022-05-03 13:42:40 +0800

    Update pwdtest.js

--- pwdtest.js
+++ pwdtest.js
@@ -4,7 +4,7 @@ if(window.navigator.onLine==true){
       if(document.getElementById("pwd").value=="hellofn!@#"){
           location.replace('https://scratch-cmd.github.io/clientdisplay/inbellycraft');
           alert('欢迎来到肚子世界！');
-  }}else{
+  }else{
     alert('你的身份不对啊');
   }else{
     alert('请保持网络连接再登录！');
